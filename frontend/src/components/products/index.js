ca07896aade4fd931b751fb491a5a3e6bd8061cd
Mónica Bernal <mok2710@hotmail.com>
2022-08-18 15:22:22 -0500

    conecting back and front

--- frontend/src/components/products/index.js
+++ frontend/src/components/products/index.js
@@ -3,7 +3,34 @@ import Products from "./Products.json";
 import Product from "./Product";
 import { ProductStyled } from "./ProductStyled";
 
+
+const baseURL = `http://localhost:8080/products`
+
+async function getProduct() {
+  try {
+    const response = axios({
+      url: `${baseURL}/products`,
+      method: 'GET'
+    })
+    return response
+  } catch (error) {
+
+  }
+
+}
+
+
 const ProductList = () => {
+  const [product, setProduct] = useState(Products);
+
+  useEffect(() => {
+    async function LoadProduct() {
+      const response = await getProduct();
+      return response
+    }
+    LoadProduct()
+  })
+
 
   return (
     <ProductStyled>
@@ -11,7 +38,7 @@ const ProductList = () => {
         {Products.map((product, index) => {
           return (
             <Product
-              key = {index}
+              key={index}
               crimg={product.crimg}
               category={product.category}
               title={product.title}
