b8e584fd3d33ba8103897b53d3dfc06c214058b8
jime_shk <jimenashk@gmail.com>
2022-09-20 17:57:34 +0000

    adding import

--- backend/HomeSpot/src/main/java/integrativeproject/homeSpot/security/jwt/JwtProvider.java
+++ backend/HomeSpot/src/main/java/integrativeproject/homeSpot/security/jwt/JwtProvider.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 // Clase que genera el token y valida que este bien formado y no este expirado
 
